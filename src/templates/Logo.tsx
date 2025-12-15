17edc274682778addd27868586956817ea059728
Xholysea <antoinealamfrx@gmail.com>
2025-12-15 20:14:42 +0200

    changing colors

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -5,30 +5,20 @@ type ILogoProps = {
 };
 
 const Logo = (props: ILogoProps) => {
-  const size = props.xl ? '44' : '32';
+  const size = props.xl ? '240' : '180';
   const fontStyle = props.xl
     ? 'font-semibold text-3xl'
     : 'font-semibold text-xl';
 
   return (
     <span className={`inline-flex items-center text-gray-900 ${fontStyle}`}>
-      <svg
-        className="mr-1 stroke-current text-primary-500"
-        xmlns="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=done_all"
+      <img
+        src="/logov3.png" // <-- Put your logo file path here
         width={size}
         height={size}
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M0 0h24v24H0z" stroke="none" />
-        <rect x="3" y="12" width="6" height="8" rx="1" />
-        <rect x="9" y="8" width="6" height="12" rx="1" />
-        <rect x="15" y="4" width="6" height="16" rx="1" />
-        <path d="M4 20h14" />
-      </svg>
+        className="mr-1"
+        alt="Logo"
+      />
 
       {AppConfig.site_name}
     </span>
